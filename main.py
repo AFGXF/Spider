fcf79ea70634c1efcf38db8cf085f4b523330e0c
afgxf <1612416019@qq.com>
2020-12-31 00:28:30 +0800

    Add Comments

--- main.py
+++ main.py
@@ -2,37 +2,41 @@ import requests
 
 # Events that can process and classify by open-and-closed.
 open_closed_processable = ["issues", "pulls"]
-
 # Input Owner,Repository Name and Actions.
 owner = str(input("Owner:"))
 repo = str(input("RepoName:"))
 action = str(input("Action:"))
-# Send a GET request
+# Send a GET request.
 req = requests.get("https://api.github.com/repos/" + owner + "/" + repo + "/" + action)
+# Exit with status 1 when Request error.
 if not req.ok:
     print("ERROR:Request Error")
     exit(1)
 # Parse JSON data
 obj = req.json()
-# Test
+# Test action type
 if action in open_closed_processable:
     events_open = 0
     events_closed = 0
     events = len(obj)
     for i in obj:
+        # Base information.
         print("#" + str(i["number"]) + ":" + i["title"], end='')
+        # Count status.
         if i["state"] == "open":
             events_open = events_open + 1
             print("\t[OPEN]")
         if i["state"] == "close":
             events_closed = events_closed + 1
             print("\t[CLOSED]")
+    # Additional Information
     if action == "issues":
         print(events, "issue(s),", events_open, "open,", events_closed, "closed")
     elif action == "pulls":
         print(events, "pull request(s),", events_open, "open,", events_closed, "closed")
-elif action=="releases":
+elif action == "releases":
     # TODO: Add process of Release
+    #   To be continued Next Year ! :D
     pass
 
 else:
